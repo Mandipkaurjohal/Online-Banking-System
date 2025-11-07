358292a8deaaf8dc927da1729bea742e4c1dc963
mandipkaur <you@example.com>
2025-11-07 18:54:56 -0500

    updateUser and deleteUser in UserServiceImpl

--- Online-Banking-System/src/main/java/com/banking/Online/Banking/System/service/Impl/UserServiceImpl.java
+++ Online-Banking-System/src/main/java/com/banking/Online/Banking/System/service/Impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -44,16 +45,30 @@ return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getPhoneN
 
     @Override
     public List<UserDTO> getAlluser() {
-        return List.of();
+        return userRepository.findAll().stream()
+                .map(user -> new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getRole(), user.getPhoneNumber()))
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public UserDTO updateUser(Long id, UserDTO userDTO) {
-        return null;
+        try {
+           User user = userRepository.findById(id).orElseThrow(()->new ResourcesNotFoundException("User not find : "+id));
+        user.setName(userDTO.getName());
+        user.setEmail(user.getEmail());
+        user.setRole(user.getRole());
+        user.setPhoneNumber(user.getPhoneNumber());
+        return userDTO;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public void deleteUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourcesNotFoundException("user not find :" + id));
 
+userRepository.delete(user);
     }
 }
